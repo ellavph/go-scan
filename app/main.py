2d5063ded00bd2863d9281b7c94ae400078a2925
Carlos <carlos.oliveira@inteliger.com.br>
2023-12-11 15:22:54 -0300

    - coniguração de cors para permitir requisições de qualquer origin

--- app/main.py
+++ app/main.py
@@ -2,9 +2,19 @@ import uvicorn
 from fastapi import FastAPI
 from app.user import routes
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", include_in_schema=False)
 def index():
